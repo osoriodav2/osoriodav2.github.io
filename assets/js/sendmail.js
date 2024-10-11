ebe359f4ac1e7b26c7be5faa51aa94a8c792d095
david <osoriodav2@gmail.com>
2024-10-11 16:10:15 -0500

    Update sendmail.js

--- assets/js/sendmail.js
+++ assets/js/sendmail.js
@@ -1,7 +1,5 @@
 function sendMail() {
     // Get form values
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("message").value;
 
@@ -9,9 +7,7 @@ function sendMail() {
     var mailto_link = "mailto:osoriodav2@gmail.com" +
                       "?subject=" + encodeURIComponent(subject) +
                       "&body=" + 
-                      "Name: " + encodeURIComponent(name) + "%0A" + 
-                      "Email: " + encodeURIComponent(email) + "%0A" + 
-                      "Message: " + encodeURIComponent(message);
+                      encodeURIComponent(message);
 
     // Open the mailto link
     window.location.href = mailto_link;
